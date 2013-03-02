85e114512606158c65824890cb66f495c600684c
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-02 13:44:47 +0200

    ReturnToQuoteDetailMixin

--- qnotes/apps/quotes/views.py
+++ qnotes/apps/quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import Http404
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
@@ -7,6 +7,20 @@ from django.forms import ValidationError
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from qnotes.apps.quotes.models import Quote
 from qnotes.apps.quotes.forms import QuoteForm
+
+
+class QuotesMixin(object):
+
+    def get_context_data(self, **kwargs):
+        context = super(QuotesMixin, self).get_context_data(**kwargs)
+        context['private_quotes'] = Quote.objects.filter(user=self.request.user, is_private=True)
+        return context
+
+
+class ReturnToQuoteDetailMixin(object):
+
+    def get_success_url(self):
+        return reverse('userena_profile_detail', kwargs={'username': self.request.user.username})
 
 
 class QuoteList(ListView):
@@ -27,23 +41,24 @@ class QuoteDetail(DetailView):
 
     def get_object(self):
         try:
-            quote = Quote.objects.get(user=self.request.user, pk=self.kwargs['pk'])
+            quote = Quote.objects.get(pk=self.kwargs['pk'])
             if quote.user == self.request.user:
                 return quote
         except Quote.DoesNotExist:
             raise Http404
-        return get_object_or_404(Quote, user=self.request.user, pk=self.kwargs['pk'], is_private=False)
+        return get_object_or_404(Quote, pk=self.kwargs['pk'], is_private=False)
 
 
-class QuoteCreate(CreateView):
+class QuoteCreate(ReturnToQuoteDetailMixin, CreateView):
     model = Quote
     form_class = QuoteForm
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        """ if the user is supplied, check if the user has a blog item
+        """ if the user is supplied, check if the user has a quote
             with the same slug at the same day
         """
+        '''
         if self.request.user and not form.errors and not form.instance.id:
             try:
                 quote = Quote.objects.get(quote=form.cleaned_data['quote'],
@@ -55,10 +70,11 @@ class QuoteCreate(CreateView):
                                 (_('You have already created this quote'),
                                 quote.quote))
                 raise ValidationError(msg)
+        '''
         return super(QuoteCreate, self).form_valid(form)
 
 
-class QuoteUpdate(UpdateView):
+class QuoteUpdate(ReturnToQuoteDetailMixin, UpdateView):
     model = Quote
     form_class = QuoteForm
 
