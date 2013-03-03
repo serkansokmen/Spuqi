280121a5cff715c82d7c7a3dc93a030b170f4934
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-03 19:44:33 +0200

    search sources by name in admin

--- qnotes/apps/authors/admin.py
+++ qnotes/apps/authors/admin.py
@@ -3,6 +3,6 @@ from qnotes.apps.authors.models import Author
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    search_fields = ('name',)
+    list_display = ('name', 'slug', )
+    search_fields = ('name', )
 admin.site.register(Author, AuthorAdmin)
--- qnotes/apps/sources/admin.py
+++ qnotes/apps/sources/admin.py
@@ -3,6 +3,7 @@ from qnotes.apps.sources.models import Source
 
 
 class SourceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'url', 'isbn')
-    list_display_links = ('title',)
+    list_display = ('title', 'slug', 'url', 'isbn', )
+    list_display_links = ('title', )
+    search_fields = ('title', 'url', 'isbn', )
 admin.site.register(Source, SourceAdmin)
