01160db6b959f142e9bad5429fece9a4e02aa488
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-06 03:17:08 +0200

    private quotes don't have discussion enabled

--- qnotes/apps/sources/tests.py
+++ qnotes/apps/sources/tests.py
@@ -1,15 +1,9 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class SourceFormTest(TestCase):
+
+    def test_displays_correct_fields(self):
         """
         Tests that 1 + 1 always equals 2.
         """
