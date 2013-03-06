1f3aac7a59943461270cf5688921826828e56997
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-06 20:06:31 +0200

    exclude AnonymousUser from Member list in COllectionForm

--- qnotes/apps/collections/forms.py
+++ qnotes/apps/collections/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Collection
 
 
@@ -7,3 +8,7 @@ class CollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
         exclude = ('user', 'slug', )
+
+    def __init__(self, *args, **kwargs):
+        super(CollectionForm, self).__init__(*args, **kwargs)
+        self.fields['members'].queryset = User.objects.filter(id__gt=-1)
