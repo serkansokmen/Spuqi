4f3605da097f5de8ce4b2523994fb6a9c47974d1
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-13 22:46:19 +0200

    local db name change

--- qnotes/settings/dev.py
+++ qnotes/settings/dev.py
@@ -24,7 +24,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'qnotes',
+        'NAME': 'qnotes_heroku',
         'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
