a350d8e35fcbdb43ed158e8c7634f38bf8138058
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-13 23:11:59 +0200

    database settings

--- qnotes/settings/prod.py
+++ qnotes/settings/prod.py
@@ -47,13 +47,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 ########## DATABASE CONFIGURATION
-# DATABASES = postgresify()
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+DATABASES = postgresify()
 ########## END DATABASE CONFIGURATION
 
 
