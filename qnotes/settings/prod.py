bdf8b19cb64853d73844294c6e003250cf727b52
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-13 22:46:34 +0200

    heroku db setup

--- qnotes/settings/prod.py
+++ qnotes/settings/prod.py
@@ -49,7 +49,13 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 ########## DATABASE CONFIGURATION
-DATABASES = postgresify()
+# DATABASES = postgresify()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ########## END DATABASE CONFIGURATION
 
 
