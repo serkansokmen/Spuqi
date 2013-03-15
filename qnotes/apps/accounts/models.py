be9fe47b2c1d86e3024b758ab2b3a325fc234bec
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-15 13:14:45 +0200

    added mugshot to user model

--- qnotes/apps/accounts/models.py
+++ qnotes/apps/accounts/models.py
@@ -1,85 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
 
 
-class SiteUserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=SiteUserManager.normalize_email(email),
-            first_name=first_name,
-            last_name=last_name,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, first_name, last_name, password):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
-class SiteUser(AbstractBaseUser):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-        db_index=True,
+class SiteUser(AbstractUser):
+    mugshot = models.ImageField(
+        _('Cluster logo'),
+        upload_to='mugshots',
+        blank=True,
+        null=True
     )
-    first_name = models.CharField(_('First Name'), max_length=120)
-    last_name = models.CharField(_('Last Name'), max_length=120)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    objects = SiteUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-
-    def get_full_name(self):
-        # The user is identified by their email address
-        return self.email
-
-    def get_short_name(self):
-        # The user is identified by their email address
-        return self.email
-
-    def __unicode__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
