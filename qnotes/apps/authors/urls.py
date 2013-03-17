0480ff9fcc0edd4f5d909c8b5d3999add9502c63
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-17 21:32:20 +0200

    pep8 lint error fixed

--- qnotes/apps/authors/urls.py
+++ qnotes/apps/authors/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import *
 from django.contrib.auth.decorators import login_required
 from .views import AuthorList, AuthorDetail, AuthorCreate, AuthorUpdate, AuthorDelete
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns(
+    '',
     url(r'^$', login_required(AuthorList.as_view()), name='author_list'),
     url(r'^new/$', login_required(AuthorCreate.as_view()), name='author_add'),
     url(r'^(?P<slug>[\d\w\_\-]+)/$', login_required(AuthorDetail.as_view()), name='author_detail'),
     url(r'^(?P<slug>[\d\w\_\-]+)/edit/$', login_required(AuthorUpdate.as_view()), name='author_edit'),
     url(r'^(?P<slug>[\d\w\_\-]+)/delete/$', login_required(AuthorDelete.as_view()), name='author_delete'),
-
 )
