980c210aa0330a1ea0a6cbe297f578f62a463517
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-19 22:14:44 +0200

    upload_to path

--- spuqi/apps/accounts/models.py
+++ spuqi/apps/accounts/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
-from django.conf import settings
 
 
 class SiteUser(AbstractUser):
     avatar = models.ImageField(
         _('Avatar'),
-        upload_to='%savatars' % settings.MEDIA_URL,
+        upload_to='uploads/%Y/%m/%d/',
         blank=True,
         null=True
     )
