fb646838c764c205936b09ee47494336f89806ff
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-19 22:10:07 +0200

    upload avatar to MEDIA_URL

--- spuqi/apps/accounts/models.py
+++ spuqi/apps/accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 
 class SiteUser(AbstractUser):
     avatar = models.ImageField(
         _('Avatar'),
-        upload_to='avatars',
+        upload_to='%savatars' % settings.MEDIA_URL,
         blank=True,
         null=True
     )
