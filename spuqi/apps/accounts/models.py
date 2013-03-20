fd1777b10c34ec605f9e432f7beddf14b8cc7c9b
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-20 11:38:18 +0200

    avatar upload dir name

--- spuqi/apps/accounts/models.py
+++ spuqi/apps/accounts/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext as _
 class SiteUser(AbstractUser):
     avatar = models.ImageField(
         _('Avatar'),
-        upload_to='uploads/%Y/%m/%d/',
+        upload_to='avatars/%Y/%m/%d/',
         blank=True,
         null=True
     )
