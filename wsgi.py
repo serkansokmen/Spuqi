d5896857dead920139e51e2f7204f074a1ac7524
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-20 11:43:47 +0200

    typo fix for wsgi settings

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qnotes project.
+WSGI config for spuqi project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qnotes.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spuqi.settings.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
