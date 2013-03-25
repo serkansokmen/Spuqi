9583d21f96b80f58f23f02776e4a5a0d087a3f6d
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-25 15:54:26 +0200

    js app layout with knockback

--- spuqi/apps/quotes/urls.py
+++ spuqi/apps/quotes/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import *
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 from .views import (
     QuoteList, QuoteDetail, QuoteCreate, QuoteUpdate, QuoteDelete
 )
@@ -8,6 +9,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', login_required(QuoteList.as_view()), name='quote_list'),
     url(r'^new/$', login_required(QuoteCreate.as_view()), name='quote_add'),
+    url(r'^app/$', TemplateView.as_view(template_name='quotes/app.html'), name='quotes_app'),
     url(r'^(?P<slug>[\d\w\_\-]+)/$', login_required(QuoteDetail.as_view()), name='quote_detail'),
     url(r'^(?P<slug>[\d\w\_\-]+)/edit/$', login_required(QuoteUpdate.as_view()), name='quote_edit'),
     url(r'^(?P<slug>[\d\w\_\-]+)/delete/$', login_required(QuoteDelete.as_view()), name='quote_delete'),
