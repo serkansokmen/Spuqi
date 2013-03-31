ae42a920b5c7d40a3ba95b13101fa1e2c7e8101f
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-03-31 16:30:00 +0300

    ISBN validation fixes #11

--- spuqi/apps/sources/forms.py
+++ spuqi/apps/sources/forms.py
@@ -1,4 +1,6 @@
+import pyisbn
 from django import forms
+from django.utils.translation import ugettext as _
 from .models import Source
 
 
@@ -7,3 +9,19 @@ class SourceForm(forms.ModelForm):
     class Meta:
         model = Source
         exclude = ('user', 'slug', )
+
+    def clean_isbn(self):
+        # validate isbn
+        isbn_str = self.cleaned_data['isbn']
+        if len(isbn_str) > 0:
+            try:
+                isbn = pyisbn.Isbn(isbn_str)
+            except pyisbn.IsbnError:
+                raise forms.ValidationError(
+                    _('ISBN number must contain only digit-numbers'))
+            else:
+                if not isbn.validate():
+                    raise forms.ValidationError(
+                        _('A valid ISBN number is required'))
+                return isbn.isbn
+        return isbn_str
